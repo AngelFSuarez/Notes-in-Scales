498b5f9e725ee18fd4fa32b41abb87168bfc34f5
Angel Suarez <afsuarez91@gmail.com>
2025-06-26 21:11:05 -0400

    Indexed root note and added the major scale steps to rootnote

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ print("Note Selected: ", note)
 
 all_notes = ['C', 'C#', 'D', 'D#', 'E', 'F',
              'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
 flat_map = {
     "Db": "C#",
     "Eb": "D#",
@@ -18,9 +21,21 @@ if note not in flat_map and note not in all_notes:
     sys.exit(1)
 
 # Major scale formula: 2 = whole step, 1 = half step
-major_scale = [2,2,1,2,2,2,1]
+major_scale_steps = [2,2,1,2,2,2,1]
 
 # We need to loop through all notes from index of (root note) note
 # print root note
 # Add the major scale formula to index pointer, mod 12 to loop back to tonic/root (just incase)
 # Account for validating user input for only natural notes
+# Major scale loop through each major scale step and add to root index
+
+rootIndex = all_notes.index(note)
+print(all_notes[rootIndex])
+
+for major_scale_step in major_scale_steps:
+    rootIndex += major_scale_step
+    if rootIndex > len(all_notes) - 1:
+        rootIndex %= 12
+    print(all_notes[rootIndex])
+
+
